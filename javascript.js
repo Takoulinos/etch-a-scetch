ddd353a81ca6b693150c52f0364df9d40dc2840e
Takoulinos <theo100theo@yahoo.gr>
2022-06-13 13:16:51 +0300

    Completed optional challenge with random colors that 'darken' with each pass until they become black.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,5 @@
-const container = document.querySelector('.container');
-resetGrid(16);
-
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', function() {resetGrid(prompt('Enter how many squares each side will have. (max 100)'))});
-
-
 function resetGrid(squaresPerSide) {
+    const container = document.querySelector('.container');
     if (squaresPerSide > 100) squaresPerSide = 100;
     container.innerHTML = '';
     for (i=0; i<squaresPerSide; i++){
@@ -14,16 +8,43 @@ function resetGrid(squaresPerSide) {
         container.appendChild(row);
         for (j=0; j<squaresPerSide; j++) {
             const square = document.createElement('div');
-            square.classList.add('square', 'unhovered');
+            square.classList.add('square',);
+            square.setAttribute('data-timeshovered' , '0');
             row.appendChild(square);
         }
     }
-    const squares = document.querySelectorAll('.unhovered');
-    squares.forEach(square => square.addEventListener('mouseover', toggleHovered));
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', hover));
 }
 
-
-function toggleHovered(e) {
-    this.setAttribute("style", "background-color: black;");
-    this.classList.remove('unhovered');
+function hover(e) {
+    this.dataset.timeshovered = parseInt(this.dataset.timeshovered) + 1 ;
+    if (this.dataset.timeshovered === '1') {
+        const r = Math.floor(Math.random()*256);
+        const g = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+        this.setAttribute("style", `background-color: rgb(${r}, ${g}, ${b});`);
+        const redStep = Math.ceil(r/10);
+        const greenStep = Math.ceil(g/10);
+        const blueStep = Math.ceil(b/10);
+        this.setAttribute("data-stepred", `${redStep}`);
+        this.setAttribute("data-stepgreen", `${greenStep}`);
+        this.setAttribute("data-stepblue", `${blueStep}`);
+    }
+    else {
+        const initialColor = this.style.backgroundColor;
+        let r = initialColor.slice(initialColor.indexOf('(') + 1, initialColor.indexOf(','));
+        let g = initialColor.slice(initialColor.indexOf(' ') + 1, initialColor.lastIndexOf(','));
+        let b = initialColor.slice(initialColor.lastIndexOf(' ') + 1, initialColor.indexOf(')'));
+        r = parseInt(r) - parseInt(this.dataset.stepred);
+        g = parseInt(g) - parseInt(this.dataset.stepgreen);
+        b = parseInt(b) - parseInt(this.dataset.stepblue);
+        this.setAttribute("style", `background-color: rgb(${r}, ${g}, ${b});`);
+    }
 }   
+
+
+
+resetGrid(16);
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', function() {resetGrid(prompt('Enter how many squares each side will have. (max 100)'))});
